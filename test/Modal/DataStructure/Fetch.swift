39b2daf140408de72e56d234758a338f7d9657e5
sanj <antarpunitsingh@gmail.com>
2019-08-06 03:46:32 -0400

    First comit

---
+++ test/Modal/DataStructure/Fetch.swift
@@ -0,0 +1,21 @@
+//
+//  Fetch.swift
+//  test
+//
+//  Created by Antarpunit Singh on 2012-07-16.
+//  Copyright © 2019 AntarpunitSingh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+ class Fetch {
+    // Storing fetched photolibrary images
+    static var imageData: [UIImage] = []
+    
+    //Storing fetched network gifs
+    static var trendingGifURL = [DataObject]()
+    
+    //Storing fonts
+    static let font = UIFont.familyNames
+}
---
+++ test/Modal/HelperFiles/ImageDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ImageDelegate.swift
+//  test
+//
+//  Created by Antarpunit Singh on 2012-07-21.
+//  Copyright © 2019 AntarpunitSingh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+protocol ImageDelegate: class {
+    // pass the the image or any variable you want in the func params
+    func imagePassed(image: UIImage)
+    func colorPassed(color: UIColor)
+}
